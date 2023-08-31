b23d288ed99b5fbd7750b4e72fe01ac05ed005c2
BaekUiHeon <qordmlgjs1@gmail.com>
2023-08-31 17:02:29 +0900

    수업내용 log4j추가, 카카오페이 테스트 , rootContext추가
    수업내용 log4j추가, 카카오페이 테스트 , rootContext추가

---
+++ src/main/java/kh/spring/gaji/paycontroller/PayController.java
@@ -0,0 +1,30 @@
+package kh.spring.gaji.paycontroller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class PayController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PayController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@GetMapping("/paytest")
+	public String home() {
+		return "payment/paytest";
+	}
+}
