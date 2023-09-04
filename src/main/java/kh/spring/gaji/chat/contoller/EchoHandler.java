32c4f60bc13c14eab4bd419f4c342c802c24fb33
bellho <1313kjh@gmail.com>
2023-09-04 17:29:33 +0900

    add ResourceConfig.java
    bean객체 관리를 위하여 추가

--- src/main/java/kh/spring/gaji/chat/contoller/EchoHandler.java
+++ src/main/java/kh/spring/gaji/chat/contoller/EchoHandler.java
@@ -3,6 +3,6 @@ package kh.spring.gaji.chat.contoller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("echo")
-public class EchoHandler extends {
+public class EchoHandler {
 	
 }
