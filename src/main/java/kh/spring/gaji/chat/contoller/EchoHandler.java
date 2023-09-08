13e5dedb0e445ef7ac8dce16233d8f83ac34d876
bellho <1313kjh@gmail.com>
2023-09-08 09:05:00 +0900

    Update EchoHandler.java, servlet-context.xml Add chatroom.jsp
    - EchoHandler.java 파일에 TextWebSocketHandler 상속을
      받아 servlet-context.xml에서 나오는 오류를 해결
    - 채팅 관련된 chatroom.jsp를 최초 생성

--- src/main/java/kh/spring/gaji/chat/contoller/EchoHandler.java
+++ src/main/java/kh/spring/gaji/chat/contoller/EchoHandler.java
@@ -1,8 +1,21 @@
 package kh.spring.gaji.chat.contoller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@RequestMapping("echo")
-public class EchoHandler {
+@RequestMapping("/echo")
+public class EchoHandler extends TextWebSocketHandler {
 	
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    }
+	
+	// websocket 연결 종료 시 
+	@Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+ 
+    }
 }
--- src/main/webapp/WEB-INF/views/chatting/chatroom.jsp
+++ src/main/webapp/WEB-INF/views/chatting/chatroom.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,16 @@
 <title>chatting room</title>
 </head>
 <body>
-
+	<textarea rows="5" cols="30" id="msgArea">
+	</textarea>
+	<!-- 채팅 내용이 올라갈 화면 -->
+	<br> 메시지 :
+	<input type="text" id="message">
+	<!-- 메시지를 보내는 부분 -->
+	<br> 상대 아이디 :
+	<input type="text" id="sellerId">
+	<!-- TODO 김종호 유저 아이디가 자동 할당되게 수정해야함 -->
+	<br>
+	<input type="button" value="전송" id="btnSend">
 </body>
 </html>
