a0ab1332e50ce7badd674de97bc9b971cfe3773c
Juni0505 <dudwns050505@gmail.com>
2023-09-14 18:05:00 +0900

    .
    커밋후 오류

---
+++ src/main/java/kh/spring/gaji/administer/model/dto/BlockUserListDto.java
@@ -0,0 +1,20 @@
+package kh.spring.gaji.administer.model.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class BlockUserListDto {
+	private String bannedId; // 회원아이디 10자이내 영어로
+	private String administerId; // 관리자ID
+	private String reasonForBlocking; // 정지사유
+	private LocalDateTime createdAt; // 생성일자
+	private int reportId; // 신고대상ID
+	private int reportClass; // (1:상품,2:커뮤니티,3:댓글)
+	
+	
+}
