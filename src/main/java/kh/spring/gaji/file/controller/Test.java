8e3de44fc5cacaa5ada2a63b6836ef3e4b249b81
Juni0505 <dudwns050505@gmail.com>
2023-09-18 15:16:51 +0900

    Rename class
    클래스명 대문자 수정

--- src/main/java/kh/spring/gaji/file/controller/Test.java
+++ src/main/java/kh/spring/gaji/file/controller/Test.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
