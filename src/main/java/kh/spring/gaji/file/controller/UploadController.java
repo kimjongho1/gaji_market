80bfee0be3fb447b44d6c876b92261db26b950d7
shin4sjh <shinsharejh@gmail.com>
2023-09-20 10:01:25 +0900

    SUPER BUILD TEST
    CSS TEST

--- src/main/java/kh/spring/gaji/file/controller/UploadController.java
+++ src/main/java/kh/spring/gaji/file/controller/UploadController.java
@@ -26,6 +26,10 @@ public class UploadController {
 	public String showUploadForm() {
 		return "filetest"; // 이미지 업로드 폼을 표시할 JSP 페이지로 이동
 	}
+	@GetMapping("/superce")
+	public String showUploadForm1() {
+		return "superce"; // 이미지 업로드 폼을 표시할 JSP 페이지로 이동
+	}
 	@GetMapping("/address")
 	public String address() {
 		return "address/addresstest"; // 이미지 업로드 폼을 표시할 JSP 페이지로 이동
