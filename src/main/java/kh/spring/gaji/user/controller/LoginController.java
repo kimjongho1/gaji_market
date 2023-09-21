94fb5649cacb56dd460e40a686db72abe1014aec
Juni0505 <dudwns050505@gmail.com>
2023-09-21 14:33:21 +0900

    .
    .

--- src/main/java/kh/spring/gaji/user/controller/LoginController.java
+++ src/main/java/kh/spring/gaji/user/controller/LoginController.java
@@ -1,6 +1,5 @@
 package kh.spring.gaji.user.controller;
 
-public class LoginController {
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/kh/spring/gaji/user/controller/UserController.java
+++ src/main/java/kh/spring/gaji/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package kh.spring.gaji.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
