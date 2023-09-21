d46470cfe8ed7c7f9ca3d7f0d68978d54647c31c
BaekUiHeon <qordmlgjs1@gmail.com>
2023-09-21 14:29:50 +0900

    .
    .

--- src/main/java/kh/spring/gaji/HomeController.java
+++ src/main/java/kh/spring/gaji/HomeController.java
@@ -19,23 +19,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
 
 	// 추후 수정 : 김종호 추가(230923)
 		@GetMapping("/chat")
