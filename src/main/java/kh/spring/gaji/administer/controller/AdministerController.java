9744581e961d05fc2ebcbb00475919fbd82b3a5d
Juni0505 <dudwns050505@gmail.com>
2023-09-21 14:30:27 +0900

    contorller create
    임시 커밋 생성 중 요구사항에 맞는 네이밍 다시 검토

---
+++ src/main/java/kh/spring/gaji/administer/controller/AdministerController.java
@@ -0,0 +1,5 @@
+package kh.spring.gaji.administer.controller;
+
+public class AdministerController {
+
+}
---
+++ src/main/java/kh/spring/gaji/user/controller/UserController.java
@@ -0,0 +1,5 @@
+package kh.spring.gaji.user.controller;
+
+public class UserController {
+
+}
