5b4d0d5f177d80fd1a29bbc41cc6aa7333b2626b
bellho <1313kjh@gmail.com>
2023-09-25 12:19:18 +0900

    Update chat.js
    - 메시지를 보내면 메시지 창에 내용 출력

--- src/main/webapp/resources/js/chat.js
+++ src/main/webapp/resources/js/chat.js
@@ -5,10 +5,10 @@ document.querySelector('.person[data-chat=person2]').classList.add('active')
  * friends, chat 객체 생성
  */
 let friends = {
-    list: document.querySelector('ul.people'),
-    all: document.querySelectorAll('.left .person'),
-    name: ''
-  },
+  list: document.querySelector('ul.people'),
+  all: document.querySelectorAll('.left .person'),
+  name: ''
+},
   chat = {
     container: document.querySelector('.container .right'),
     current: null,
@@ -19,7 +19,7 @@ let friends = {
 /** 김종호(230922)
 'mousedown' 이벤트가 발생할 때, 해당 요소(f)가 'active' 클래스를 포함하고 있지 않은 경우에만 
 setActiveChat 함수를 호출하여 요소를 활성화
-*/ 
+*/
 friends.all.forEach(f => {
   f.addEventListener('mousedown', () => {
     f.classList.contains('active') || setAciveChat(f)
@@ -48,32 +48,85 @@ function setAciveChat(f) {
 }
 
 
-$(function(){
+$(function () {
+  const username = "${pageContext.request.userPrincipal.name}";
+  const websocket = new WebSocket("ws://localhost:8090/spring1/echo");
 
-  const username="${pageContext.request.userPrincipal.name}";
-  
+  websocket.onopen = function (e) {
+    console.log('WebSocket 연결이 열렸습니다.');
+  };
+
+  websocket.onmessage = function (e) {
+    console.log('WebSocket 메시지 수신:', e.data);
+    // 서버에서 수신한 메시지를 화면에 표시
+    displayReceivedMessage(e.data);
+  };
+
+  websocket.onclose = function (e) {
+    console.log('WebSocket 연결이 닫혔습니다.');
+  };
+
+  websocket.onerror = function (e) {
+    console.error('WebSocket 오류:', e);
+  };
+
   $("#button-send").on("click", (e) => {
-   send();
+    send();
   });
 
+  // 엔터 키 입력시 send() 실행
+  $("#msg").on("keydown", (e) => {
+    if (e.key === "Enter") {
+      send();
+    }
+  });
 
-const websocket = new WebSocket("ws://localhost:8090/spring1/echo");
+  function send() {
+    const msgInput = document.getElementById("msg");
+    const message = msgInput.value.trim(); // 입력된 메시지를 가져옴
 
-// websocket.onmessage은 서버에 메시지가 도착했을 때 호출되는 이벤트 핸들러
-// onMessage은 websocket.onmessage을 참조함
-websocket.onmessage = onMessage;
+    if (message === "") {
+      // 메시지가 비어있으면 전송하지 않음
+      return;
+    }
 
-function send(){
+    // 현재 활성화된 채팅 창을 찾음
+    const activeChat = document.querySelector('.chat.active-chat');
 
-  let msg = document.getElementById("msg");
+    if (!activeChat) {
+      // 현재 활성화된 채팅 창이 없으면 아무 작업도 수행하지 않음
+      return;
+    }
 
-  console.log(username + ":" + msg.value);
-  // websocket.send은 웹소켓으로 textMessage객체의 값을 보냄
-  websocket.send(username + ":" + msg.value);
-  //textMessage 객체의 값 초기화
-  msg.value = '';
-}
+    // 메시지를 채팅 창에 추가 (자신이 보낸 메시지)
+    const messageElement = document.createElement('div');
+    messageElement.className = 'bubble me'; // 여기서는 자신이 보낸 메시지로 가정
+    messageElement.textContent = message;
 
-// 9.25 메시지 화면 출력 작업 재개 
+    activeChat.appendChild(messageElement);
 
+    // 입력된 메시지 초기화
+    msgInput.value = '';
+
+    // WebSocket을 통해 메시지 서버로 전송
+    websocket.send(username + ":" + message);
+  }
+
+  // WebSocket을 통해 받은 메시지를 화면에 표시하는 함수
+  function displayReceivedMessage(message) {
+    // 현재 활성화된 채팅 창을 찾음
+    const activeChat = document.querySelector('.chat.active-chat');
+
+    if (!activeChat) {
+      // 현재 활성화된 채팅 창이 없으면 아무 작업도 수행하지 않음
+      return;
+    }
+
+    // 메시지를 채팅 창에 추가 (상대방이 보낸 메시지)
+    const messageElement = document.createElement('div');
+    messageElement.className = 'bubble you'; // 상대방이 보낸 메시지
+    messageElement.textContent = message;
+
+    activeChat.appendChild(messageElement);
+  }
 });
