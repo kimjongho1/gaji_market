836728d5d733e233e2642045d141159c4df52d8c
bellho <1313kjh@gmail.com>
2023-09-26 15:32:48 +0900

    Update UserController.java

--- src/main/java/kh/spring/gaji/user/controller/UserController.java
+++ src/main/java/kh/spring/gaji/user/controller/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
 	
 	@GetMapping("/signup")
 	public String signup() {	// 개인회원가입
-		service.checkMobileNumber(null)
+		service.checkMobileNumber(null);
 		
 		
 		return "user/signup";
