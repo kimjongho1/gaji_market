a0729b4a9a5d43cd7cdf0db212b412c31ae0aecc
bellho <1313kjh@gmail.com>
2023-09-27 14:26:39 +0900

    Update LoginController.java

--- src/main/java/kh/spring/gaji/user/controller/LoginController.java
+++ src/main/java/kh/spring/gaji/user/controller/LoginController.java
@@ -6,18 +6,27 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login() {		//  로그인페이지 -- 관리자 로그인 페이지도 따로 만들 예정 추후 논의
-		return "user/login";
+	public ModelAndView login(ModelAndView mv) {		//  로그인페이지 -- 관리자 로그인 페이지도 따로 만들 예정 추후 논의
+		mv.setViewName("user/login");
+		return mv;
 	}
 	
 	@PostMapping("/logout")		//  로그아웃 -> 메인페이지로 이동
-	public String logout(HttpServletRequest request) {
+	public ModelAndView logout(ModelAndView mv, HttpServletRequest request) {
+		mv.setViewName("redirect:/main");
 		request.getSession().invalidate();
-		return "redirect:/main";
+		return mv;
+	}
+	
+	@GetMapping("/login/error")
+	public ModelAndView viewLoginError(ModelAndView mv) {
+		mv.setViewName("user/login/error");
+		return mv;
 	}
 }
