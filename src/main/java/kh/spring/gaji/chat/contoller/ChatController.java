c9b706e92de7a8f2899ae12b2ba2920174e3c015
bellho <1313kjh@gmail.com>
2023-10-14 14:40:06 +0900

    Update chat
    - message, chatId를 ajax로 통해 controller 값 전달

--- src/main/java/kh/spring/gaji/chat/contoller/ChatController.java
+++ src/main/java/kh/spring/gaji/chat/contoller/ChatController.java
@@ -5,7 +5,6 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,39 +23,29 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class ChatController {
-	
+
 	@Autowired
 	private ChatService chatServiceImpl;
-	
+
 	@GetMapping("/chat")
-	public ModelAndView selectChatHome(
-			ModelAndView mv,
-			Principal principal
-			) {
+	public ModelAndView selectChatHome(ModelAndView mv, Principal principal) {
 		// userId 값으로 조회하기 때문에 변수 선언
 		String userId = principal.getName();
 		// 채팅방 리스트 출력
 		List<ChatRoomDto> result1 = chatServiceImpl.getChatRoom(userId);
-		for(int i=0; i<result1.size(); i++) {
+		for (int i = 0; i < result1.size(); i++) {
 			result1.get(i).setChatInfo(chatServiceImpl.getChatInfo(result1.get(i).getChatId()));
 		}
-		mv.addObject("chatRoomList",result1);
+		mv.addObject("chatRoomList", result1);
 		mv.setViewName("chat/chatroom");
 		log.info("getChatRoom 실행");
 		return mv;
 	}
 
-	//채팅 선택
+	// 채팅 선택
 	@GetMapping("/selectRoom")
 	@ResponseBody
-	public String
-//	List<ChatMessageDto>
-	selectRoom(
-//			ModelAndView mv,
-			Principal principal,
-//			@RequestParam(name = "chatId") 
-			String chatId
-			) {
+	public String selectRoom(Principal principal, String chatId) {
 		String userId = principal.getName();
 		List<ChatMessageDto> result1 = chatServiceImpl.getChatMessage(Integer.parseInt(chatId));
 		System.out.println(result1);
@@ -66,4 +55,10 @@ public class ChatController {
 		return new Gson().toJson(result1);
 	}
 
+	@GetMapping("/insultChat")
+	@ResponseBody
+	public void selectRoom(String msg, String chatId) {
+		System.out.println(msg);
+		System.out.println(chatId);
+	}
 }
