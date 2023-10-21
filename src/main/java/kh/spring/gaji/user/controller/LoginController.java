5f6e7e9c19307b5bb5097a5228c4aa4a0c38269e
Juni0505 <dudwns050505@gmail.com>
2023-10-21 21:33:34 +0900

    Update LoginController.java
    로그인값 있을시에 접근 제한 메인페이지로 이동

--- src/main/java/kh/spring/gaji/user/controller/LoginController.java
+++ src/main/java/kh/spring/gaji/user/controller/LoginController.java
@@ -9,14 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public ModelAndView login(ModelAndView mv, Principal principal) {		//  로그인페이지 -- 관리자 로그인 페이지도 따로 만들 예정 추후 논의
+	public ModelAndView login(ModelAndView mv, Principal principal, RedirectAttributes ra) {		//  로그인페이지 -- 관리자 로그인 페이지도 따로 만들 예정 추후 논의
 		if(principal != null) {
-			
+			ra.addFlashAttribute("msg","로그아웃 후 진행해주세요.");
+			mv.setViewName("redirect:/");
+			return mv;
 		}
 		mv.setViewName("user/login");
 		return mv;
